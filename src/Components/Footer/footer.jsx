4e487b7e538cd40f064776b1bc7136281ae5ef64
sahiljarngal <97387822+sahiljarngal@users.noreply.github.com>
2024-07-12 01:24:42 +0530

    icicc

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,56 +1,54 @@
 import React from "react";
-import classes from "./footer.module.css"
+import classes from "./footer.module.css";
 
 function Footer() {
-    return ( 
+  return (
     <div className={classes.footer}>
-        <div className={ classes.fmain }>
+      <div className={classes.fmain}>
+        {/* footer left  */}
+        <div className={classes.fleft}>
+          <div className={classes.details}>
+            <h1>Contact Details</h1>
+            <p>icicc.ui@gmail.com</p>
+          </div>
 
-            {/* footer left  */}
-            <div className={classes.fleft}>
+          <div className={classes.socialMedia}>
+            <h1>Social Media</h1>
 
-            <div className={classes.details}>
-                <h1>Contact Details</h1>
-                <p>icicc.ui@gmail.com</p>
+            <div className={classes.social}>
+              <a href="">Gmail</a>
+              <a href="">Facebook</a>
+              <a href="">LinkedIn</a>
             </div>
-
-            <div className={classes.socialMedia}>
-                <h1>Social Media</h1>
-
-                <div className={classes.social}>
-                   <a href="">Gmail</a>
-                   <a href="">Facebook</a>
-                   <a href="">LinkedIn</a>
-                   </div>
-            </div>
-                  
-            </div>
-
-            {/* footer right  */}
-           <div className={classes.fright}>
-           <div className={classes.links}>
-                <h1>Quick Links</h1>
-                <p>Call for papers</p>
-                <p>Important Dates</p>
-                <p>Conference Venue</p>
-                <p>Downloads</p>
-                <p>EasyChair Submission</p>
-            </div>
-           </div>
-
-
+          </div>
         </div>
 
-<div className={classes.line}>
-      <hr />
+        {/* footer right  */}
+        <div className={classes.fright}>
+          <div className={classes.links}>
+            <h1>Quick Links</h1>
+            <p>Call for papers</p>
+            <p>Important Dates</p>
+            <p>Conference Venue</p>
+            <p>Downloads</p>
+            <p>EasyChair Submission</p>
+          </div>
+        </div>
       </div>
 
-        <div className={classes.f_lower}>
-            <h3>Copyright © International Conference on Innovative Computing and Communication | All Rights Reserved |</h3>
-            <p>Site By: Akshun Verma</p>
-        </div>
+      <div className={classes.line}>
+        <hr />
+      </div>
+
+      <div className={classes.f_lower}>
+        <h3>
+          Copyright © International Conference on Innovative Computing and
+          Communication | All Rights Reserved |
+        </h3>
+        <p>Site By: Sahil</p>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Footer;
